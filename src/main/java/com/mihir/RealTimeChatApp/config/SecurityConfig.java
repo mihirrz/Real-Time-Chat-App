e0226e650ece91eec9bed64fabb77d98ba0e8c4a
Mihir <mihirraj52@gmail.com>
2024-12-26 17:05:44 +0530

    Added WebConfig

--- src/main/java/com/mihir/RealTimeChatApp/config/SecurityConfig.java
+++ src/main/java/com/mihir/RealTimeChatApp/config/SecurityConfig.java
@@ -12,15 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
-
-//import java.util.Arrays;
-//import java.util.List;
 
 @Configuration
 @EnableWebSecurity
